25be399b6872ee797ef3ddaaa67e381f4b079aa7
Tim Abraldes <tim@timabraldes.com>
2016-02-17 21:01:08 -0800

    Clean up urlJobProcessor.js

--- crawler/urlJobProcessor.js
+++ crawler/urlJobProcessor.js
@@ -5,6 +5,10 @@ import { default as urlModule } from 'url';
 const probes = [];
 
 function configure(opts) {
+  if (!opts.probes || !opts.probes.length) {
+    return Promise.reject(new Error('At least one urlJobProcessor probe must be specified'));
+  }
+
   const ret = [Promise.resolve()];
 
   opts.probes.forEach((probeOpts) => {
@@ -12,6 +16,8 @@ function configure(opts) {
       return Promise.reject(new Error('Probe without name encountered'));
     }
 
+    // We allow the probe to be specified by name or by path. If specified by name,
+    // we expect it to be a known subdir.
     const path = probeOpts.path || `./urlJobProcessorProbes/${probeOpts.name}.js`;
     const probeModule = require(path).default;
 
@@ -37,22 +43,31 @@ function configure(opts) {
 
 function fetchAllResources(urlArg) {
   const ret = { scripts: [] };
-  const urlStr = `http://${urlArg}`;
-  const url = urlModule.parse(urlStr);
+  const url = urlModule.parse(`http://${urlArg}`);
 
+  // TODO: Pass user agent in headers
   return fetch(`${url.href}`, { headers: '' }).then((response) => {
+    // We pass along the response object that was returned after
+    // our fetch of the main page.
     ret.mainResponse = response;
     return response.text();
   }).then((htmlText) => {
+    // We have to pass along the response body since it can no longer
+    // be retrieved from the response.
     ret.mainResponseHtml = htmlText;
+
     const $ = cheerio.load(htmlText);
     const promises = [];
     $('script').each((index, elem) => {
       if (!$(elem).attr('src')) {
+        // For inline scripts, just append them to the array of scripts
+        // that we'll pass to the probes
         ret.scripts.push($(elem).text());
       } else {
+        // For scripts with a `src` attribute, we must fetch them here
         let src = $(elem).attr('src');
         if (src[0] === '/') {
+          // Deal with relative paths in `src` attributes
           src = `${url.protocol}//${url.host}${src}`;
         }
         promises.push(fetch(src).then((response) => {
